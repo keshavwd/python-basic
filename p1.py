64a2b9f16dc6dc4665e9c271c85f843a35f43022
keshavwd <keshav.wd@gmail.com>
2026-04-15 12:05:17 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,138 @@
+#Hello Python
+print("Hello Python")
+
+
+#Name Input
+'''
+name = str(input("Write your name : "))
+print("Welcome", name)
+'''
+
+#Number as Input
+'''
+numberFloat = float(input("Enter the number : "))
+print("Your entered number is", numberFloat)
+'''
+
+#Two numbers as input
+'''
+numA = int(input("First number is : "))
+numB = int(input("Secont number is : "))
+print(numA)
+print(numB)
+'''
+
+#Print name and age
+'''
+name = str(input("Type your name : "))
+age = int(input("Enter your age :"))
+print("My name is", name, "and my age is", age)
+'''
+
+#Square of integer
+#Doubt
+'''
+num = int(input("Enter the number to get its square :"))
+print(num*num)
+'''
+
+
+#Sum of integer
+'''
+numA = int(input("Enter the first number : "))
+numB = int(input("Enter the second number : "))
+print(numA + numB)
+'''
+
+#ASM of integers
+'''
+numA = int(input("Enter the first number : "))
+numB = int(input("Enter the second number : "))
+print(numA + numB)
+print(numA - numB)
+print(numA * numB)
+'''
+
+#Print a number
+'''
+numA = int(input("Enter the nimber : "))
+print("The number entered is", numA)
+'''
+
+
+#Print int double
+'''
+numA = int(input("Enter the integer : "))
+print(numA * 2)
+'''
+
+#Float value
+'''
+numA = float(input("Enter float number : "))
+print(numA)
+'''
+
+#Sum of float numbers
+'''
+numA = float(input("Enter the first number : "))
+numB = float(input("Enter the second number : "))
+print(numA + numB)
+'''
+
+
+#Half of float number
+'''
+numA = float(input("Enter the number : "))
+print(numA/2)
+'''
+
+#Area of rect or square
+'''
+numA = float(input("Enter the length : "))
+numB = float(input("Enter the width : "))
+print(numA * numB, "cm sq")
+'''
+
+#Area of circle
+'''
+numA = float(input("Enter the radius : "))
+areaOfCircle = 3.14 * (numA * numA)
+print("Area of circle is :", areaOfCircle,"cm sq")
+'''
+
+#Print float and int
+'''
+numA = int(input("Enter the integer : "))
+numB = float(input("Enter the float number : "))
+print("Integer is", numA, "and float is", numB)
+'''
+
+#Avg of marks
+'''
+numA = int(input("Enter total marks obtained : "))
+numB = int(input("Enter total number of subjects : "))
+avg = (numA/(numB*100))*100
+print("Your average is",avg)
+'''
+
+#Shop calculation
+'''
+numA = float(input("Price per unit :"))
+numB = int(input("Quantity of items in numbers : "))
+value = numA * numB
+print("Total cost of items is Rs",value)
+'''
+
+#Sum of float and int
+'''
+numA = float(input("Enter the first value : "))
+numB = int(input("Enter the second value : "))
+sum = numA + numB
+print(sum)
+'''
+
+#Take salary
+numA = float(input("Enter your salary : "))
+numB = int(input("Enter your bonus : "))
+takeSal = numA + numB
+print("Your total salary is", takeSal)
