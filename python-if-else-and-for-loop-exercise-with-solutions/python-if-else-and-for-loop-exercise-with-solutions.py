1a8daf5d22ef7a2e8b8b784722e4cb12952a2fab
keshavwd <keshav.wd@gmail.com>
2026-09-11 21:27:50 +0530

    update

--- python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
+++ python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
@@ -13,7 +13,32 @@ for i in range(-10,0,1):
 '''
 
 #Display a message “Done” after successful execution of for loop
+'''
 for i in range(0,5):
     print( i)
 else:
     print("Done")
+'''
+
+#Calculate the sum of all numbers from 1 to N
+'''
+num = int(input("Enter the last digit for sum from 0 to: "))
+s = 0
+s1 = 0
+count = 0
+while count <= num:
+    s = s+count
+    count += 1
+print(s)
+
+for i in range(num+1):
+    s1 +=i
+print(s1)
+'''
+
+#Print multiplication table of a given number
+'''
+n = int(input("Enter the number to have the table: "))
+for i in range(1,10+1):
+    print(f"{n}*{i}={n*i}")
+'''
