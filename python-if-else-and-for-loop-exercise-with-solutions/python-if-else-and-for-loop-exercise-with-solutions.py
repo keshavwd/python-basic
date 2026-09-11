c043d7986c099cd4cf8b7fbca3e6b6c2d0d95e55
keshavwd <keshav.wd@gmail.com>
2026-09-11 23:06:16 +0530

    new

--- python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
+++ python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
@@ -42,3 +42,46 @@ n = int(input("Enter the number to have the table: "))
 for i in range(1,10+1):
     print(f"{n}*{i}={n*i}")
 '''
+
+#Calculate the cube of all numbers from 1 to a given number
+'''
+n = int(input("Enter the number: "))
+for i in range(1,n+1):
+    print(f"Current Number is : {i} and the cube is {i**3}")
+'''
+
+#Display numbers from a list using a loop
+'''
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for i in numbers:
+    if i >500:
+        break
+    if i > 150:
+        continue
+    if i%5==0:
+        print(i)
+'''
+
+#Count occurrences of a specific element in a list
+'''
+list1 = [10, 20, 10, 30, 10, 40, 50]
+tar = 10
+count = 0
+for i in list1:
+    if i == tar:
+        count += 1
+print(count)
+'''
+
+#Print elements from a list present at odd index positions
+'''
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+nlist = []
+for i in range(len(my_list)):
+    if i%2!=0:
+        nlist.append(my_list[i])
+print(nlist)
+'''
+
+#Print list in reverse order using a loop
+list1 = [10, 20, 30, 40, 50]
