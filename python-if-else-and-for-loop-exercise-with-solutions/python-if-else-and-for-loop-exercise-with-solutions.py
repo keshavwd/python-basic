e06265047ea068429cca9176686760e1874a69e9
keshavwd <keshav.wd@gmail.com>
2026-09-11 14:53:36 +0530

    3 exercise done

--- python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
+++ python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
@@ -1,1 +1,19 @@
-print("Hello")
+#Print first 10 natural numbers using while loop
+'''
+n = 1
+while n <=10:
+    print(n)
+    n += 1
+'''
+
+#Display numbers from -10 to -1 using for loop
+'''
+for i in range(-10,0,1):
+    print(i)
+'''
+
+#Display a message “Done” after successful execution of for loop
+for i in range(0,5):
+    print( i)
+else:
+    print("Done")
