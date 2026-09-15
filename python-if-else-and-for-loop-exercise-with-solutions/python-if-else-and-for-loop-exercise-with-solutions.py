f2e3156a501c74b8c807d72a23d209958e7ac84f
keshavwd <keshav.wd@gmail.com>
2026-09-15 11:38:53 +0530

    New data

--- python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
+++ python-if-else-and-for-loop-exercise-with-solutions/python-if-else-and-for-loop-exercise-with-solutions.py
@@ -275,3 +275,21 @@ for i in range(0,5):
 '''
 
 #Print pyramid pattern of stars
+'''
+n = int(input("Enter the number of row to print: "))
+for i in range(0,n+1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+for i in range(n-1,0,-1):
+    for k in range(0,i):
+        print("*",end=" ")
+    print()
+'''
+
+#Print full multiplication table (1 to 10)
+n = 10
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i*j,end="\t")
+    print()
